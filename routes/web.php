0ed90dcb68251f6636b965a7b194c785ccaaa316
RamintaRam <raminta5@yahoo.com>
2017-05-10 09:03:27 +0300

    Routes created: admin & user

--- routes/web.php
+++ routes/web.php
@@ -90,9 +90,88 @@ Route::group(['prefix' => 'admin'], function () {
         Route::delete('/delete', ['as' => 'app.pizzas.delete', 'uses' => 'DTPizzasController@destroy']);
     });
 
+    /** Roles routes  */
+
+    Route::group(['prefix' => 'roles'], function () {
+        Route::get('/', ['as' => 'app.roles.list', 'uses' => 'DTRolesController@index']);
+        Route::get('/create', ['uses' => 'DTRolesController@create']);
+        Route::post('/create', ['as' => 'app.roles.create', 'uses' => 'DTRolesController@store']);
+
+    });
+
+    Route::group(['prefix' => '{id}'], function () {
+
+        Route::get('/', ['uses' => 'DTRolesController@show']);
+        Route::get('/edit', ['uses' => 'DTRolesController@edit']);
+        Route::post('/edit', ['as' => 'app.roles.update', 'uses' => 'DTRolesController@update']);
+        Route::delete('/delete', ['as' => 'app.roles.delete', 'uses' => 'DTRolesController@destroy']);
+    });
+
+    /** Users routes  */
+
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', ['as' => 'app.users.list', 'uses' => 'DTUsersController@index']);
+        Route::get('/create', ['uses' => 'DTUsersController@create']);
+        Route::post('/create', ['as' => 'app.users.create', 'uses' => 'DTUsersController@store']);
+
+    });
+
+    Route::group(['prefix' => '{id}'], function () {
+
+        Route::get('/', ['uses' => 'DTUsersController@show']);
+        Route::get('/edit', ['uses' => 'DTUsersController@edit']);
+        Route::post('/edit', ['as' => 'app.users.update', 'uses' => 'DTUsersController@update']);
+        Route::delete('/delete', ['as' => 'app.users.delete', 'uses' => 'DTUsersController@destroy']);
+    });
+
+
+    /** Users routes  */
+
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', ['as' => 'app.users.list', 'uses' => 'DTUsersController@index']);
+        Route::get('/create', ['uses' => 'DTUsersController@create']);
+        Route::post('/create', ['as' => 'app.users.create', 'uses' => 'DTUsersController@store']);
+
+    });
+
+    Route::group(['prefix' => '{id}'], function () {
+
+        Route::get('/', ['uses' => 'DTUsersController@show']);
+        Route::get('/edit', ['uses' => 'DTUsersController@edit']);
+        Route::post('/edit', ['as' => 'app.users.update', 'uses' => 'DTUsersController@update']);
+        Route::delete('/delete', ['as' => 'app.users.delete', 'uses' => 'DTUsersController@destroy']);
+    });
+
+    /** Permissions routes  */
+
+    Route::group(['prefix' => 'permissions'], function () {
+        Route::get('/', ['as' => 'app.permissions.list', 'uses' => 'DTPermissionsController@index']);
+        Route::get('/create', ['uses' => 'DTPermissionsController@create']);
+        Route::post('/create', ['as' => 'app.permissions.create', 'uses' => 'DTPermissionsController@store']);
+
+    });
+
+    Route::group(['prefix' => '{id}'], function () {
+
+        Route::get('/', ['uses' => 'DTPermissionsController@show']);
+        Route::get('/edit', ['uses' => 'DTPermissionsController@edit']);
+        Route::post('/edit', ['as' => 'app.permissions.update', 'uses' => 'DTPermissionsController@update']);
+        Route::delete('/delete', ['as' => 'app.permissions.delete', 'uses' => 'DTPermissionsController@destroy']);
+    });
+
+}); /** END of ADMIN routes*/
 
 
 
+/**  USER Permissions */
 
-}); /** END of ADMIN */
+Route::group(['prefix' => 'user'], function () {
+    Route::group(['prefix' => 'pizzas'], function () {
+        Route::get('/', ['as' => 'app.pizzas.index', 'uses' => 'DTPizzasController@index']);
+        Route::get('/create', ['uses' => 'DTPizzasController@create']);
+        Route::post('/create', ['as' => 'app.pizzas.create', 'uses' => 'DTPizzasController@store']);
+        Route::get('/', ['uses' => 'DTPizzasController@show']);
+    });
+});
+/** END of USER routes*/
 
